71a99f106dbc86e23f9c2d080714bdacb6ff3ac1
cpethica <charles@pethica.net>
2022-05-20 00:54:13 +0200

    initial commit

---
+++ Tennis.cpp
@@ -0,0 +1,345 @@
+/*
+  Tennis.cpp - Library for instances of LED strip tennis game.
+  Charlie Pethica - May 2022
+*/
+#include "Arduino.h"
+#include "Tennis.h"
+#include "FastLED.h"
+Tennis::Tennis(int playerBtnPin[], int playerLedPin[], const uint8_t dataPin, unsigned int numLeds)
+{
+	pinMode(playerBtnPin[0], INPUT);   // PINs for buttons
+	pinMode(playerBtnPin[1], INPUT);
+	_dataPin = dataPin;
+	const unsigned int _numLeds = numLeds;
+	_playerBtnPin = playerBtnPin;
+	_playerLedPin = playerLedPin;
+
+	// dataPin must be defined before runtime so define which one is used here...
+	switch(_dataPin){
+        case 16:
+            FastLED.addLeds<WS2812B, 16, GRB>(_leds, _numLeds);
+            break;
+        case 17:
+            FastLED.addLeds<WS2812B, 17, GRB>(_leds, _numLeds);
+            break;
+        default:
+      		Serial.println("Unsupported Pin");
+      		break;
+    }
+
+}
+
+
+// Menu (set brightness and start game)
+void Tennis::menu(){
+  // implement later	
+   digitalWrite(_playerLedPin[0], HIGH);  // activate both LEDs
+   digitalWrite(_playerLedPin[1], HIGH);
+
+  GeneratePlayField(maxBright);         // show play-field
+  FastLED.show();
+
+  // set brightness (button player 0)
+  if (buttonBounce(0, 5) == true)
+  {
+    maxBright += 50;
+    if (maxBright > 255)
+    {
+      maxBright = 55;
+    }
+    GeneratePlayField(maxBright);
+    FastLED.show();
+
+    while (buttonBounce(0, 5) == true) {}   // wait for button release
+  }
+
+  // start game (button player 1)
+  if (buttonBounce(1, 5) == true)
+  {
+    digitalWrite(_playerLedPin[0], LOW);     // deactivate both LEDs
+    digitalWrite(_playerLedPin[1], LOW);
+    _activeGame = true;
+
+    while (buttonBounce(1, 5) == true) {}   // wait for button release
+
+    game();
+  }
+}
+
+// function to debounce button (true == pressed, false == not pressed)
+boolean Tennis::buttonBounce(byte button, byte bounceTime) // bounce the button
+{
+  boolean result = false;
+
+  if (digitalRead(_playerBtnPin[button]) == LOW)
+  {
+    delay (bounceTime);
+    if (digitalRead(_playerBtnPin[button]) == LOW)
+    {
+      result = true;
+    }
+  }
+  return result;
+}
+
+void Tennis::game()
+{
+  while (_activeGame)
+  {
+
+    _gameSpeed = gameSpeedMin;           // set starting game speed
+    _ballSpeed = _gameSpeed;              // set starting ball speed
+    memset(_playerButtonPressed, -1, sizeof(_playerButtonPressed)); // clear keypress
+
+    GeneratePlayField(_scoreDimBright);  // show gamefield with dimmed score
+    FastLED.show();
+
+    InitializePlayers();  // set the player-settings -> wait for keypress to start game
+
+    GameLoop();           // main loop: move ball -> ball left gamefield? -> check keypress -> check if keypress in endzone -> change direction
+    
+    CheckScore();         // check who made score and show it
+
+    CheckWinner();        // check if we have a winner
+  }
+}
+
+
+void Tennis::InitializePlayers()
+{
+
+  if (_playerStart == 0)   // initialize for player 0
+  {
+    _ballDir = 1;          // set ball direction
+    _ballPos = 0;          // set startposition of ball
+
+    digitalWrite(_playerLedPin[0], HIGH);              // activate LED
+    while (digitalRead(_playerBtnPin[0]) == HIGH) {} // wait for button
+    digitalWrite(_playerLedPin[0], LOW);               // deactivate LED
+  }
+  else                        // initialize for player 1
+  {
+    _ballDir = -1;             // set ball direction
+    _ballPos = NUM_LEDS - 1;   // set startposition of ball
+
+    digitalWrite(_playerLedPin[1], HIGH);              // activate LED
+    while (digitalRead(_playerBtnPin[1]) == HIGH) {} // wait for button
+    digitalWrite(_playerLedPin[1], LOW);               // deactivate LED
+  }
+}
+
+
+void Tennis::GameLoop()
+{
+  while (true)    // loop, exit with break when one player made a score
+  {
+
+    if (millis() - _previousMoveMillis > _ballSpeed)  // time to move ball
+    {
+      _previousMoveMillis = millis();
+
+      GeneratePlayField(_scoreDimBright);
+
+      _ballPos += _ballDir;
+      if (_ballPos < 0 || _ballPos >= NUM_LEDS) // ball left endzone?
+      {
+        break;                // leave loop -> one player made a score
+      }
+      _leds[_ballPos] = CHSV(0, 0, maxBright);    // generate ball (white)
+      FastLED.show();
+    }
+
+    CheckButtons();     // check keypress
+
+    // fix positions of keypress for testing
+    // if (ballPos == 3) playerButtonPressed[0] = 3;
+    // if (ballPos == 59) playerButtonPressed[1] = 59;
+
+    CheckButtonPressedPosition();
+
+  } // end of while-loop
+}
+
+
+// *** check if buttons pressed
+void Tennis::CheckButtons()
+{
+  for (int i = 0; i < 2; i++)
+  {
+    // player pressed button?
+    if (_playerButtonPressed[i] == -1 && digitalRead(_playerBtnPin[i]) == LOW && (_ballDir + 1) / 2 == i)
+      // (ballDir + 1) / 2 == i  -->  TRUE, when:
+      // ballDir == -1  AND  i = 0  -->  player 0 is active player
+      // ballDir == +1  AND  i = 1  -->  player 1 is active player
+      // only the button-press of the active player is stored
+    {
+      _playerButtonPressed[i] = _ballPos;   //store position of pressed button
+      _previousButtonColor = _ballPos;
+      _previousButtonColor = playerColor[i];
+      _previousButtonMillis = millis(); // store time when button was pressed
+    }
+  }
+}
+
+
+// *** check, if button was pressed when ball was in endzone and if so, change direction of ball
+void Tennis::CheckButtonPressedPosition()
+{
+  if (_ballDir == -1 && _playerButtonPressed[0] <= endZoneSize - 1 && _playerButtonPressed[0] != -1)
+  {
+    ChangeDirection();
+  }
+
+  if (_ballDir == +1 && _playerButtonPressed[1] >= NUM_LEDS - endZoneSize)
+  {
+    ChangeDirection();
+  }
+}
+
+
+void Tennis::ChangeDirection()
+{
+  _ballDir *= -1;
+  _gameSpeed -= gameSpeedStep;
+  _ballSpeed = _gameSpeed;
+  if (_ballPos == 0 || _ballPos == NUM_LEDS - 1)  // triggered on first or last segment
+  {
+    _ballSpeed -= ballBoost0;      // Super-Boost
+  }
+
+  if (_ballPos == 1 || _ballPos == NUM_LEDS - 2)  // triggered on second or forelast segment
+  {
+    _ballSpeed -= ballBoost1;      // Boost
+  }
+
+  _ballSpeed = max(_ballSpeed, ballSpeedMax);                     // limit the maximum ballspeed
+  memset(_playerButtonPressed, -1, sizeof(_playerButtonPressed)); // clear keypress
+}
+
+
+void Tennis::CheckScore()
+{
+  _previousButtonPos = -1;     // clear last ball-position at button-press
+
+  // check who made score
+  if (_ballPos < 0)            // player1 made the score
+  {
+    _playerScore[1] += 1;      // new score for player1
+
+    GeneratePlayField(maxBright);   // show new score full bright
+    BlinkNewScore(NUM_LEDS / 2 - 1 + _playerScore[1], playerColor[1]); // blink last score
+
+    _playerStart = 1;          // define next player to start (player, who made the point)
+  }
+  else                        // player0 made the score
+  {
+    _playerScore[0] += 1;      // new score for player0
+
+    GeneratePlayField(maxBright);   // show new score full bright
+    BlinkNewScore(NUM_LEDS / 2 - _playerScore[0], playerColor[0]); // blink last score
+
+    _playerStart = 0;          // define next player to start (player, who made the point)
+  }
+
+  GeneratePlayField(maxBright);     // show new score full bright
+  FastLED.show();
+
+  delay(1000);
+}
+
+
+void Tennis::CheckWinner()
+{
+  // check if we have a winner
+  if (_playerScore[0] >= winRounds || _playerScore[1] >= winRounds)
+  { // we have a winner!
+
+    _activeGame = false;
+
+    FastLED.clear();
+    Rainbow(_playerScore[0] > _playerScore[1]); // TRUE if player 0 won; FALSE when player 1 won
+
+    memset(_playerScore, 0, sizeof(_playerScore));  // reset all scores
+
+    _playerStart = abs(_playerStart - 1);   // next game starts looser
+
+  }
+}
+
+void Tennis::GeneratePlayField(byte bright)
+{
+  FastLED.clear();        // clear all
+  GenerateEndZone();      // generate endzone
+  GenerateScore(bright);  // generate actual score
+  GenerateLastHit();      // generate mark of position of last button-press
+}
+
+void Tennis::GenerateEndZone()
+{
+  for (int i = 0; i < endZoneSize; i++)
+  {
+    _leds[i] = CHSV(endZoneColor, 255, maxBright);
+    _leds[NUM_LEDS - 1 - i] = CHSV(endZoneColor, 255, maxBright);
+  }
+}
+
+
+void Tennis::GenerateScore(int bright)
+{
+  int i;
+
+  // Player 0
+  for (i = 0; i < _playerScore[0]; i++)
+  {
+    _leds[NUM_LEDS / 2 - 1 - i] = CHSV(playerColor[0], 255, bright);
+  }
+
+  // Player 1
+  for (i = 0; i < _playerScore[1]; i++)
+  {
+    _leds[NUM_LEDS / 2 + i] = CHSV(playerColor[1], 255, bright);
+  }
+}
+
+
+void Tennis::GenerateLastHit()
+{
+  if (_previousButtonPos != -1 && _previousButtonMillis + 500 > millis())
+  {
+    _leds[_previousButtonPos] = CHSV(_previousButtonColor, 255, _previousButtonBright);
+  }
+}
+
+void Tennis::BlinkNewScore(int pos, byte color)
+{
+  for (int i = 1; i <= 4; i++)
+  {
+    _leds[pos] = CHSV(color, 255, (i % 2) * maxBright);  // blink LED 2 times (1-0-1-0)
+    FastLED.show();
+    delay(300);
+  }
+}
+
+
+void Tennis::Rainbow(boolean won)
+{
+  for (int k = 0; k < 3; k++)   // 3 rounds rainbow
+  {
+    for (int j = 0; j <= 255; j++)
+    {
+      for (int i = 0; i < NUM_LEDS / 2; i++)
+      {
+        if (won == true)    // player 0 won --> Rainbow left
+        {
+          _leds[i] = CHSV(((i * 256 / NUM_LEDS) + j) % 256, 255, maxBright);
+        }
+        else                // player 1 won --> Rainbow right
+        {
+          _leds[NUM_LEDS - i - 1] = CHSV(((i * 256 / NUM_LEDS) + j) % 256, 255, maxBright);
+        }
+      }
+      FastLED.show();
+      delay(7);
+    }
+  }
+}
---
+++ Tennis.h
@@ -0,0 +1,72 @@
+/*
+  Tennis.h - Library for instances of LED strip tennis game.
+  Charlie Pethica - May 2022
+*/
+
+#ifndef Tennis_h
+#define Tennis_h
+#include "Arduino.h"
+#include "FastLED.h"
+class Tennis {
+// define variables and function accessible from sketch here:
+public:
+  
+  Tennis(int playerBtnPin[], int playerLedPin[], const uint8_t dataPin, unsigned int numLeds);          // class constructor - setup class from skectch
+  
+  // public variables within class
+  int maxBright = 155; // max brightness
+  int gameSpeedMin    =       50; // min game-speed
+  int gameSpeedMax    =        5; // max game-speed
+  int gameSpeedStep   =        1; // fasten up when change direction
+  int ballSpeedMax    =        1; // max ball-speed
+  int ballBoost0      =       25; // superboost on last position
+  int ballBoost1      =       15; // boost on forelast position
+  byte playerColor[2]  =  {0, 96}; // red & green
+  int winRounds       =       10; // x winning rounds for winning game
+  int endZoneSize     =        12; // size endzone
+  int endZoneColor    =      160; // color endzone
+  int NUM_LEDS        =      120; // number of leds per strip
+
+  //public functions within class
+  void menu();    // menu and start game function
+  boolean buttonBounce(byte button, byte bounceTime);
+  void game();
+  void InitializePlayers();
+  void GameLoop();
+  void CheckButtons();
+  void CheckButtonPressedPosition();
+  void ChangeDirection();
+  void CheckScore();
+  void CheckWinner();
+  void GeneratePlayField(byte bright);
+  void GenerateEndZone();
+  void GenerateScore(int bright);
+  void GenerateLastHit();
+  void BlinkNewScore(int pos, byte color);
+  void Rainbow(boolean won);
+// variables and functions only used by class defined here:
+
+private:
+  
+  int* _playerBtnPin;               // declaration of buttons variable within class only
+  int* _playerLedPin;
+  int _dataPin;
+  boolean _activeGame = false;                 // true when game is active
+  unsigned long _previousMoveMillis;           // time of last ball move
+  unsigned long _previousButtonMillis;         // time of last button-press
+  int _playerButtonPressed[2];                 // ball-position where button was pressed; „-1“ button not pressed
+  int _previousButtonPos = -1;                 // position of last button-press
+  byte _previousButtonColor;                   // color of field for last Button-press
+  int _playerScore[2];                         // actual Score
+  byte _playerStart;                           // who starts game
+  int _gameSpeed;                              // actual game-speed
+  int _ballDir = 1;                            // direction, ball is moving (+/- 1)
+  int _ballPos;                                // actual ball-position
+  int _ballSpeed;                              // actual ball-speed (higher = slower)
+  byte _previousButtonBright = maxBright / 2;  // bright of marked last position when button pressen
+  byte _scoreDimBright       = maxBright / 4;  // bright of dimmed score
+  CRGB _leds[120];
+
+
+};
+#endif
